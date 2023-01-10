923d2c3ea62900f798e9b512cc90b99a93d0acc0
maxueming <maxueming@kuaishou.com>
2023-01-10 16:37:46 +0800

    feat: 1

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   themeConfig: {
     name: 'Greenet',
+    logo: 'https://raw.githubusercontent.com/paiDaXing-web/learning-materials-pdf/main/logo.png',
     nav: [
       { title: '算法', link: '/algorithm' },
       { title: '挑战', link: '/challenge' },
