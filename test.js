cfb971cdfc3384222f384ea71dc4a6da963e06e9
maxueming <maxueming@kuaishou.com>
2023-01-19 20:05:24 +0800

    feat: 更新首页

--- test.js
+++ test.js
@@ -1,12 +1,52 @@
-var twoSum = function (nums, target) {
-  let map = {};
-  for (var i = 0; i < nums.length; i++) {
-    console.log(map);
-    if (map[target - nums[i]] !== undefined) {
-      return [map[target - nums[i]], i];
+var addTwoNumbers = function (l1, l2) {
+  const a1 = l1.reverse();
+  const a2 = l2.reverse();
+  let res = [];
+  let a1len = a1.length - 1,
+    a2len = -1;
+
+  let isTen = false;
+  for (let i = 0; i <= Math.max(a1len, a2len); i++) {
+    if (a1len - i >= 0 && a2len - i >= 0) {
+      if (!isTen) {
+        if (a1[a1len - i] + a2[a2len - i] < 10) {
+          res.unshift(a1[a1len - i] + a2[a2len - i]);
+          isTen = false;
+        } else {
+          res.unshift((a1[a1len - i] + a2[a2len - i]) % 10);
+          isTen = true;
+        }
+      } else {
+        if (a1[a1len - i] + a2[a2len - i] + 1 < 10) {
+          res.unshift(a1[a1len - i] + a2[a2len - i] + 1);
+          isTen = false;
+        } else {
+          res.unshift((a1[a1len - i] + a2[a2len - i] + 1) % 10);
+          isTen = true;
+        }
+      }
     } else {
-      map[nums[i]] = i;
+      const a3 = getLoner(a1, a2);
+      if (isTen) {
+        if (a3[a3.length - 1 - i] + 1 < 10) {
+          res.unshift(a3[a3.length - 1 - i] + 1);
+          isTen = false;
+        } else {
+          res.unshift((a3[a3.length - 1 - i] + 1) % 10);
+          isTen = true;
+        }
+      } else {
+        res.unshift(a3[a3.length - 1 - i]);
+        isTen = false;
+      }
     }
   }
+  console.log(res);
 };
-twoSum([2, 7, 11, 15], 9);
+var l1 = [9, 9, 9, 9, 9, 9, 9, 9, 9],
+  l2 = [9, 9, 9, 9, 9, 9];
+addTwoNumbers(l1, l2);
+
+function getLoner(l1, l2) {
+  return l1.length > l2.length ? l1 : l2;
+}
