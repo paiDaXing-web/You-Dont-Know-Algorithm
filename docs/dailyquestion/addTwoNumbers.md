5b2f8e8b10c0ff91d29f17de3ebb43df0f7ed93b
maxueming <maxueming@kuaishou.com>
2023-01-21 23:55:39 +0800

    feat: leecode 第二题

--- docs/dailyquestion/addTwoNumbers.md
+++ docs/dailyquestion/addTwoNumbers.md
@@ -68,3 +68,37 @@ tocDepth: 4
   `O(max(m,n))`，假设 `l1` 的长度是 `m`， `l2` 的长度是 `n`，时间复杂度就是两者的最大值。
 - 空间复杂度:
   O(1)
+
+  #### code
+
+```javascript
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  const dummy = new ListNode(0);
+  let curr = dummy;
+  let carry = 0;
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    let x = l1 ? l1.val : 0;
+    let y = l2 ? l2.val : 0;
+    let sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+
+  return dummy.next;
+};
+```
--- test.js
+++ test.js
@@ -1,13 +1,39 @@
 var addTwoNumbers = function (l1, l2) {
   const dummy = new ListNode(0);
+  let curr = dummy;
+  let carry = 0;
   console.log(dummy);
-};
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    let x = l1 ? l1.val : 0;
+    let y = l2 ? l2.val : 0;
+    let sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
   }
-}
-var l1 = [9, 9, 9, 9, 9, 9, 9, 9, 9],
-  l2 = [9, 9, 9, 9, 9, 9];
+};
+// class ListNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+// function ListNode(val, next) {
+//   this.val = val === undefined ? 0 : val;
+//   this.next = next === undefined ? null : next;
+// }
+var l1 = {
+  val: 9,
+  next: {
+    val: 9,
+    next: {
+      val: 9,
+      next: null,
+    },
+  },
+};
+l2 = { val: 9, next: null };
 addTwoNumbers(l1, l2);
