42ba2a59908d612e5ebcfa03026fdef22bc75f8e
maxueming <maxueming@kuaishou.com>
2023-01-31 16:31:50 +0800

    feat: vue2 模版默认依赖更改为2.7x

--- test.js
+++ test.js
@@ -1,39 +1,13 @@
-var addTwoNumbers = function (l1, l2) {
-  const dummy = new ListNode(0);
-  let curr = dummy;
-  let carry = 0;
-  console.log(dummy);
-
-  while (l1 !== null || l2 !== null || carry !== 0) {
-    let x = l1 ? l1.val : 0;
-    let y = l2 ? l2.val : 0;
-    let sum = x + y + carry;
-    carry = Math.floor(sum / 10);
-    curr.next = new ListNode(sum % 10);
-    curr = curr.next;
-    l1 = l1 ? l1.next : null;
-    l2 = l2 ? l2.next : null;
-  }
-};
-// class ListNode {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
-// }
-// function ListNode(val, next) {
-//   this.val = val === undefined ? 0 : val;
-//   this.next = next === undefined ? null : next;
-// }
-var l1 = {
-  val: 9,
-  next: {
-    val: 9,
-    next: {
-      val: 9,
-      next: null,
-    },
-  },
-};
-l2 = { val: 9, next: null };
-addTwoNumbers(l1, l2);
+let dest, src, result;
+/**
+ * 简单复制
+ */
+dest = {};
+src = { id: 'src' };
+result = Object.assign(dest, src);
+// Object.assign 修改目标对象
+// 也会返回修改后的目标对象
+console.log(dest === result); // true
+console.log(dest !== src); // true
+console.log(result); // { id: src }
+console.log(dest); // { id: src }
